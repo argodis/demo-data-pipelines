4f9e66a51bbce3dd894b04a54c353555f4623bbe
David Höppner <david@argodis.de>
2022-02-07 16:14:06 +0100

    Spark workflow stop to clean data in landing

---
+++ alpaca/alpaca_spark_job_clean.py
@@ -0,0 +1,56 @@
+# coding: utf-8
+
+import configparser
+import sys
+
+
+#
+# Part of the daily executed job to download Alpaca trading data.
+#
+
+
+#
+# Global variables
+#
+
+#sys.path.insert(0, "/dbfs/Users/david@argodis.de/github/demo/")
+
+
+DB_CLUSTER_ID = None
+LOCAL = True
+#CONFIG_FILE = "/Users/davidhoeppner/Work/argodis/git/demo-data-pipelines/alpaca/alpaca.ini"
+CONFIG_FILE = "/dbfs/Users/david@argodis.de/github/demo/alpaca.ini"
+
+#
+# Are we running in Databricks or locally?
+#
+try:
+    spark
+except NameError:
+    from pyspark.sql import SparkSession
+
+    spark = SparkSession.builder.appName("Alpaca CLEAN").getOrCreate()
+else:
+    DB_CLUSTER_ID = spark.conf.get("spark.databricks.clusterUsageTags.clusterId")
+    LOCAL = False
+
+
+if __name__ == "__main__":
+    config = configparser.ConfigParser()
+    config.read(CONFIG_FILE)
+
+    environment = "local.trades" if LOCAL else "databricks.trades"
+
+    trades_landing_path = config[environment]["TradesLanding"]
+    trades_clean_path = config[environment]["TradesClean"]
+
+    print(trades_landing_path)
+    print(trades_clean_path)
+
+    print(LOCAL)
+
+    df = spark.read.parquet(trades_landing_path)
+    df.printSchema()
+
+    df.repartition('date').write.mode('overwrite').partitionBy('date').parquet(trades_clean_path)
+
