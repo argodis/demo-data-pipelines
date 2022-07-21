22f98ea4b9a2c5c25d25391d28bfe40fd77c2ad5
David Höppner <david@argodis.de>
2022-07-21 10:50:33 +0200

    Pass on storage location to ray cluster

--- scripts/databricks_ray_init.sh
+++ scripts/databricks_ray_init.sh
@@ -3,6 +3,7 @@
 #RAY PORT
 RAY_PORT=9339
 REDIS_PASS="d4t4bricks"
+STORAGE_PATH="/dbfs/mnt/data/alpaca/w1"
 
 # install ray
 /databricks/python/bin/pip install ray pyarrow alpaca-trade-api
@@ -14,7 +15,7 @@ REDIS_PASS="d4t4bricks"
 # If starting on the Spark worker node, connect to the head Ray node
 if [ ! -z $DB_IS_DRIVER ] && [ $DB_IS_DRIVER = TRUE ] ; then
   echo "Starting the head node"
-  ray start  --min-worker-port=20000 --max-worker-port=25000 --temp-dir="/tmp/ray" --head --port=$RAY_PORT --redis-password="$REDIS_PASS"  --include-dashboard=false --metrics-export-port=8080
+  ray start  --min-worker-port=20000 --max-worker-port=25000 --temp-dir="/tmp/ray" --head --port=$RAY_PORT --redis-password="$REDIS_PASS"  --include-dashboard=false --metrics-export-port=8080 --storage="${STORAGE_PATH}"
   
   cd /tmp
   /usr/bin/curl -LO https://github.com/prometheus/prometheus/releases/download/v2.32.1/prometheus-2.32.1.linux-amd64.tar.gz
@@ -23,5 +24,5 @@ if [ ! -z $DB_IS_DRIVER ] && [ $DB_IS_DRIVER = TRUE ] ; then
 else
   sleep 120
   echo "Starting the non-head node - connecting to $DB_DRIVER_IP:$RAY_PORT"
-  ray start  --min-worker-port=20000 --max-worker-port=25000 --temp-dir="/tmp/ray" --address="$DB_DRIVER_IP:$RAY_PORT" --redis-password="$REDIS_PASS"
+  ray start  --min-worker-port=20000 --max-worker-port=25000 --temp-dir="/tmp/ray" --address="$DB_DRIVER_IP:$RAY_PORT" --redis-password="$REDIS_PASS" --storage="${STORAGE_PATH}"
 fi
