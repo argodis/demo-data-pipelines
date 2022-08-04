234f685b59b0bb566ed6cd54b94f769949742e40
David Höppner <david@argodis.de>
2022-08-04 16:57:27 +0200

    Generate terminal graphs

--- bars/quality_gaps.py
+++ bars/quality_gaps.py
@@ -1,30 +1,60 @@
+"""
+Find gaps in minute bar data.
+"""
+
+import argparse
+import logging
+import sys
+
+from uniplot import plot
 
 import numpy as np
 import pandas as pd
 
 
+STORAGE_PATH = "/tmp/datalake/landing/alpaca/bars/minute"
+
+
 pd.set_option('display.max_rows', None)
 
-df = pd.read_parquet("/tmp/datalake/landing/alpaca/bars/historical-daily-bars/minute")
 
-df = df.between_time("9:30", "17:45")
+if __name__ == "__main__":
 
-c_df = df.loc["2022-01-06"]
-print(c_df.head(1000))
-df = df.reset_index()
-df["delta"] = df.groupby("symbol")["timestamp"].diff()
-df = df.where(df["delta"] > np.timedelta64(1, "m")).dropna()
+    parser = argparse.ArgumentParser(
+        description="Quality check time bars."
+    )
 
-df["seconds"] = df["delta"].dt.seconds
+    parser.add_argument(
+        "--storage-path",
+        type=str,
+        dest="storage_path",
+        help="Path to load data from",
+    )
 
-plot_df = df[["timestamp", "symbol", "seconds"]].copy()
+    args = parser.parse_args()
 
-plot_df.set_index(plot_df["timestamp"], inplace = True)
-plot_df = plot_df.drop(columns=["timestamp"])
+    if args.storage_path:
+        STORAGE_PATH = args.storage_path
 
-plot_df = plot_df.loc["2022-01-06"]
-plot_df = plot_df.where(plot_df["seconds"] < 1000).dropna()
-plot_df = plot_df.where(plot_df["symbol"] == "AAPL").dropna()
+    try:
+        df = pd.read_parquet(STORAGE_PATH)
+    except FileNotFoundError:
+        logging.error("Can not load data from %s", STORAGE_PATH)
+        sys.exit(1)
 
-#plot_df.head()
-#plot_df.plot(y="seconds")
+    df = df.between_time("9:30", "17:45")
+    df = df.reset_index()
+    df["delta"] = df.groupby("symbol")["timestamp"].diff()
+    df = df.where(df["delta"] > np.timedelta64(1, "m")).dropna()
+
+    df["seconds"] = df["delta"].dt.seconds
+
+    plot_df = df[["timestamp", "symbol", "seconds"]].copy()
+
+    plot_df.set_index(plot_df["timestamp"], inplace = True)
+    #plot_df = plot_df.drop(columns=["timestamp"])
+
+    for i, g in plot_df.groupby(pd.Grouper(freq='24H')):
+        if not g.empty:
+            g = g.where(g["seconds"] < 1000).dropna()
+            plot(ys=g["seconds"])
