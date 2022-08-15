56c3a8f727a29902b164e1df073fb193f98c1fad
David Höppner <david@argodis.de>
2022-08-15 16:10:09 +0200

    Change to today semantics

--- bars/download.py
+++ bars/download.py
@@ -1,8 +1,16 @@
 """
 Download bars from Alpaca.
 
+If no date range is given by the start-date and
+end-date cmd line arguments we use a today semantics
+as defined in the user/system timezone running the
+script. We will NOT start partial downloads if the
+market is still open.
+
 Should be refactored with the Ray and
-snapshot code.
+snapshot code. We also redownload data
+because we do not know the filepath
+when saving.
 """
 
 import argparse
@@ -30,7 +38,7 @@ try:
 except NameError:
     DB_CLUSTER_ID = None
     LOCAL = True
-    CONFIG_FILE = "/Users/davidhoeppner/Work/argodis/git/demo-data-pipelines/bars/downloader.ini"
+    CONFIG_FILE = "/Workspace/Repos/finance/finance/bars/downloader.ini"
 else:
     LOCAL = False
     CONFIG_FILE = "/dbfs/Users/david@argodis.de/github/demo/downloader.ini"
@@ -43,9 +51,17 @@ TODAY = datetime.datetime.now().strftime("%Y-%m-%d")
 
 if __name__ == "__main__":
 
+    if DB_CLUSTER_ID:
+        key_id = dbutils.secrets.get("dbc", "alpaca-key-id-unlimited") # pylint: disable=E0602
+        secret_key = dbutils.secrets.get("dbc", "alpaca-key-secret-unlimited") # pylint: disable=E0602
+    else:
+        key_id = os.getenv("APCA_API_KEY_ID")
+        secret_key = os.getenv("APCA_API_SECRET_KEY")
+
     parser = argparse.ArgumentParser(
         description="Alpaca bars downloader."
     )
+
 
     # Symbols
     parser.add_argument("--symbols", type=str, help="Symbols to download data for")
@@ -75,7 +91,26 @@ if __name__ == "__main__":
         help="End date",
     )
 
+    parser.add_argument(
+        "--force",
+        action="store_true",
+        help="Force partial data download."
+    )
+
+    # Storage
+    parser.add_argument("--delta-table", type=str, dest="delta_table", help="Save dataframes to a delta table")
+
     args = parser.parse_args()
+
+    PAPER_URL = "https://paper-api.alpaca.markets"
+    paper_api = REST(key_id=key_id, secret_key=secret_key, base_url=URL(PAPER_URL))
+
+    today = datetime.datetime.now()
+    logging.warning("System is using timezone: %s", today.astimezone().tzname())
+
+    timezone_est = pytz.timezone("EST")
+    timezone_utc = pytz.timezone("UTC")
+    now = datetime.datetime.now().astimezone()
 
     if args.symbols:
         if args.symbols_by_volume:
@@ -96,7 +131,37 @@ if __name__ == "__main__":
         else:
             ALPACA_START = datetime.datetime.strptime(args.start_date, "%Y-%m-%d")
     else:
-        ALPACA_START = datetime.datetime.now(pytz.timezone("EST")) - datetime.timedelta(1)
+        calendar = paper_api.get_calendar(start=today, end=today)
+        if not calendar:
+            logging.error("No calendar data for %s", today)
+            sys.exit(1)
+
+        calendar = calendar[0]
+
+        date = calendar.date.to_pydatetime()
+        market_open = date + datetime.timedelta(
+            hours=calendar.open.hour,
+            minutes=calendar.open.minute
+        )
+        market_close = date + datetime.timedelta(
+            hours=calendar.close.hour,
+            minutes=calendar.close.minute
+        )
+
+        market_close_tz = market_close.astimezone(timezone_est)
+        market_open_tz = market_open.astimezone(timezone_est)
+
+        if now < market_open_tz:
+            logging.warning("Market not open yet. Exiting")
+            if not args.force:
+                sys.exit(1)
+        elif now < market_close_tz:
+            logging.warning("Market is still open. Exiting")
+            if not args.force:
+                sys.exit(1)
+
+        ALPACA_START = market_open_tz.astimezone(timezone_utc)
+        ALPACA_END = market_close_tz.astimezone(timezone_utc)
 
     if args.end_date:
         if not args.start_date:
@@ -109,26 +174,17 @@ if __name__ == "__main__":
             sys.exit(1)
         else:
             ALPACA_END = datetime.datetime.strptime(args.end_date, "%Y-%m-%d")
-    else:
-        ALPACA_END = ALPACA_START + datetime.timedelta(1)
+
+    DELTA_TABLE = args.delta_table
 
     assert ALPACA_START
     assert ALPACA_END
+    assert ALPACA_INTERVAL
 
     ALPACA_START = ALPACA_START.strftime("%Y-%m-%d")
     ALPACA_END = ALPACA_END.strftime("%Y-%m-%d")
 
     logging.info(ALPACA_START, ALPACA_END)
-
-    if DB_CLUSTER_ID:
-        key_id = dbutils.secrets.get("dbc", "alpaca-key-id-unlimited") # pylint: disable=E0602
-        secret_key = dbutils.secrets.get("dbc", "alpaca-key-secret-unlimited") # pylint: disable=E0602
-    else:
-        key_id = os.getenv("APCA_API_KEY_ID")
-        secret_key = os.getenv("APCA_API_SECRET_KEY")
-
-    PAPER_URL = "https://paper-api.alpaca.markets"
-    paper_api = REST(key_id=key_id, secret_key=secret_key, base_url=URL(PAPER_URL))
 
     config = configparser.ConfigParser()
     config.read(CONFIG_FILE)
@@ -184,3 +240,7 @@ if __name__ == "__main__":
         df["date"] = df["timestamp"].dt.date
         df["symbol"] = symbol
         df.to_parquet(p, partition_cols=["date", "symbol"])
+
+        if DELTA_TABLE:
+            spark_df = spark.createDataFrame(df)
+            spark_df.write.format("delta").mode("append").saveAsTable(DELTA_TABLE)
